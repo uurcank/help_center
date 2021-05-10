fe5b97abb601816ab5bf2cada0cd5ab11710d158
Ugurcan Kaya <uurcan@pasilobus.com>
2021-05-10 07:57:34 -0700

    add descriptions to categories

--- app/models/support_category.rb
+++ app/models/support_category.rb
@@ -1,6 +1,9 @@
 class SupportCategory < ApplicationRecord
   extend FriendlyId
   friendly_id :name, use: :slugged
+  
+  has_many :support_threads
+  has_rich_text :description
 
   scope :sorted, ->{ order(position: :asc) }
 
